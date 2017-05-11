befbdd00241bfa4330f2f9d93602bf38cb6812d8
evgeniy.egov <evgeniy.egov@simbirsoft.com>
2017-05-11 20:30:31 +0400

    Добавили перечисление, для обозначения состояния поля

---
+++ WindowsFormsAppXO/WindowsFormsApp1/FieldState.cs
@@ -0,0 +1,21 @@
+﻿namespace WindowsFormsApp1
+{
+	/// <summary>
+	/// Зададим перечисление, характеризующие состояние поля
+	/// </summary>
+	enum FieldState
+	{
+		/// <summary>
+		/// Поле пустое
+		/// </summary>
+		Empty,
+		/// <summary>
+		/// В поле стоит крестик
+		/// </summary>
+		Field_X,
+		/// <summary>
+		/// В поле стоит нолик
+		/// </summary>
+		Field_O
+	}
+}
