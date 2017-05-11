24e776c4d490c9ae90ea76f6f91c6727369f526a
evgeniy.egov <evgeniy.egov@simbirsoft.com>
2017-05-11 20:51:21 +0400

    Добавили перечисление, фиксирующее состояние игры

---
+++ WindowsFormsAppXO/WindowsFormsApp1/GameState.cs
@@ -0,0 +1,28 @@
+﻿namespace WindowsFormsApp1
+{
+	/// <summary>
+	/// Возможные состояния игры
+	/// </summary>
+	enum GameState
+	{
+		/// <summary>
+		/// Игра не началась
+		/// </summary>
+		NotStarted,
+
+		/// <summary>
+		/// Игра в прогрессе
+		/// </summary>
+		InProgress,
+
+		/// <summary>
+		/// Игрок один выиграл
+		/// </summary>
+		Player1Win,
+
+		/// <summary>
+		/// Игрок 2 выиграл
+		/// </summary>
+		Player2Win
+	}
+}
